b729e2fbd66b3bb9b517db22d5d380a720e24d42
Raghavendra KULKARNI <RAGHAVENDRA.VK@GMAIL.COM>
2020-10-11 19:03:11 +0530

    Extend Report Enhanced

--- TestAutomationJNJ/src/test/java/Generic_Component/Base_Class.java
+++ TestAutomationJNJ/src/test/java/Generic_Component/Base_Class.java
@@ -14,17 +14,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-
-
-
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 
 
@@ -32,7 +35,7 @@ public class Base_Class {
 	
 	public static ExtentHtmlReporter htmlReporter;
 	public static ExtentReports extentreport;
-	public static ExtentTest extenttest;
+	public static ExtentTest test;
 	public WebDriver driver;
 
 	
@@ -63,6 +66,20 @@ public class Base_Class {
 		
 		
 	}
+	@AfterMethod
+    public void getResult(ITestResult result) {
+        if(result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+" FAILED ", ExtentColor.RED));
+            test.fail(result.getThrowable());
+        }
+        else if(result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" PASSED ", ExtentColor.GREEN));
+        }
+        else {
+            test.log(Status.SKIP, MarkupHelper.createLabel(result.getName()+" SKIPPED ", ExtentColor.ORANGE));
+            test.skip(result.getThrowable());
+        }
+    }
 	
 	@BeforeClass
 	public void setUp(){
@@ -77,7 +94,6 @@ public class Base_Class {
 			// TODO Auto-generated catch block
 			System.out.println("URL is not available");
 		}
-		//driver.manage().window().setSize(new Dimension(375,812));
 		System.out.println(driver.getTitle());
 		System.out.println("Setup Complete");
 		
@@ -89,18 +105,26 @@ public class Base_Class {
 	Date date =new Date();
 	SimpleDateFormat df=new SimpleDateFormat("yyyy-mm-dd-hh-mm-ss");
 	String format = df.format(date);
-	//extentreport=new ExtentReports("C:\\Selenium\\2020\\TestAutomationJNJ\\Report\\"+"BB_Project"+"-"+format+".html",false);
 	// initialize the HtmlReporter
-    htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") +"\\Report\\"+"BB_Project"+"-"+format+".html");
+    htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") +"\\Report\\"+"QC_Project"+"-"+format+".html");
+    
+    htmlReporter.config().setChartVisibilityOnOpen(true);
+    htmlReporter.config().setDocumentTitle("Extent Report");
+    htmlReporter.config().setReportName("Test Report");
+    htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+    htmlReporter.config().setTheme(Theme.STANDARD);
+    htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
     
     //initialize ExtentReports and attach the HtmlReporter
     extentreport = new ExtentReports();
     extentreport.attachReporter(htmlReporter);
+    
 	}
 	
 	@AfterSuite
     public void tearDown() {
     	//to write or update test information to reporter
+	
     	extentreport.flush();
     	driver.quit();
     }	
--- TestAutomationJNJ/src/test/java/Scenario_Component/LaunchUrl.java
+++ TestAutomationJNJ/src/test/java/Scenario_Component/LaunchUrl.java
@@ -1,47 +1,66 @@
 package Scenario_Component;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 import Generic_Component.Base_Class;
-import Generic_Component.Utility_class;
+
 
 public class LaunchUrl extends Base_Class {
 	
 	
-	//public static Logger log=Logger.getLogger(LaunchUrl.class);
+
 		  
     @Test
 	public void testLogin() throws IOException {
-    	SoftAssert sAssert= new SoftAssert();
-    	extenttest=extentreport.createTest("1");	
-		extenttest.log(Status.PASS, "Executing the TestCase "+"1" + "Order set is "+"Order_Set");
+    	test=extentreport.createTest("LoginTest");	
+		test.log(Status.PASS, "Executing the TestCase "+"1" + "Order set is "+"Order_Set");
 		//extenttest.log(Status.PASS, extenttest.addScreencastFromPath(Capture_Screenshot("1", "Order_Set"));
-		extenttest.addScreenCaptureFromPath(Capture_Screenshot("1", "Order_Set"));
-		//System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");
-		//WebDriver driver=new ChromeDriver();
-		
-		
+		test.addScreenCaptureFromPath(Capture_Screenshot("1", "Order_Set"));
+				
 		
 	}
     
+    @Test
+    public void testCase1() {
+        test = extentreport.createTest("Test Case 1", "PASSED test case");
+        Assert.assertTrue(true);
+    }
     
+    @Test
+    public void testCase2() {
+        test = extentreport.createTest("Test Case 2", "PASSED test case");
+        Assert.assertTrue(true);
+    }
     
+    @Test
+    public void testCase3() {
+        test = extentreport.createTest("Test Case 3", "PASSED test case");
+        Assert.assertTrue(true);
+    }
+     
+    @Test
+    public void testCase4() {
+        test = extentreport.createTest("Test Case 4", "PASSED test case");
+        Assert.assertTrue(false);
+    }
+     
+    @Test
+    public void testCase5() {
+        test = extentreport.createTest("Test Case 5", "SKIPPED test case");
+        throw new SkipException("Skipping this test with exception");
+    }
+    
+    @Test(enabled=false)
+	public void testCase6(){
+			test = extentreport.createTest("Test Case 6", "I'm Not Ready, please don't execute me");
+		}
    
+     
 
 }
